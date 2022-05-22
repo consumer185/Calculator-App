0f3413065a375dc63e07e2d455d67837b2adef87
consumer185 <97405992+consumer185@users.noreply.github.com>
2022-05-22 01:39:29 +0100

    added styles.css & styling

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var displayText = $("#displayText").html();
-
+var firstNumberEntered = false;
+var firstNumber = "";
+var operation = "";
+var calculation = "";
 
 $(".numeric").click(function() {
-  if ($("#displayText").html() === "0") {
+
+  if (firstNumberEntered === false) {
+
+
+    if ($("#displayText").html() === "0") {
+      $("#displayText").html("");
+      $("#displayText").append(this.innerHTML);
+    } else {
+      $("#displayText").append(this.innerHTML);
+    }
+  }
+
+  if (firstNumberEntered === true) {
     $("#displayText").html("");
-    $("#displayText").append(this.innerHTML);
-  } else {
     $("#displayText").append(this.innerHTML);
   }
 });
@@ -18,14 +31,52 @@ $(".decimalPoint").click(function() {
 
 $(".reset").click(function() {
   $("#displayText").html("0");
+  var firstNumberEntered = false;
+  var firstNumber = "";
+  var operation = "";
+  var calculation = "";
 });
 
 $(".delete").click(function() {
-
   if (document.getElementById("displayText").innerHTML.length > 1) {
     document.getElementById("displayText").innerHTML = document.getElementById("displayText").innerHTML.slice(0, -1);
   } else {
     $("#displayText").html("0");
   }
+});
 
+
+$(".operation").click(function() {
+  firstNumber = document.getElementById("displayText").innerHTML;
+  operation = this.innerHTML;
+  console.log(firstNumber + operation);
+  firstNumberEntered = true;
+});
+
+
+$(".equals").click(function() {
+  var secondNumber = document.getElementById("displayText").innerHTML;
+
+  if (operation === "/") {
+    calculation = firstNumber / secondNumber;
+  }
+
+  if (operation === "x") {
+    calculation = firstNumber * secondNumber;
+  }
+
+  if (operation === "+") {
+    var intfirstNumber = parseInt(firstNumber);
+    var intsecondNumber = parseInt(secondNumber);
+    calculation = intfirstNumber + intsecondNumber;
+  }
+
+  if (operation === "-") {
+    calculation = firstNumber - secondNumber;
+  }
+
+
+
+
+  document.getElementById("displayText").innerHTML = calculation;
 });
