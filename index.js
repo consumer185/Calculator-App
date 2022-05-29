ea02ef29c05925378720c2d76a23e3794fb1f14f
consumer185 <97405992+consumer185@users.noreply.github.com>
2022-05-29 22:52:58 +0100

    working on themes

--- index.js
+++ index.js
@@ -15,12 +15,187 @@ var currentTheme = 1;
 //
 // });
 
-$(".input-range").click(function() {
-  if (document.getElementById("input-range").value === 1) {
+// $(".reset").click(function() {
+//   if ($("#input-range").val() === 2) {
+//     alert("we're on theme 1 people!");
+//   } else {console.log("nothing doing");
+// }
+// });
+
+
+document.getElementById("input-range").oninput = function() {
+
+  if ($("#input-range").val() === "1") {
     console.log("we're on theme 1 people!");
-  } else {console.log("nothing doing");
-}
-});
+
+    $(".masterDiv").removeClass("masterDiv-theme2");
+    $(".masterDiv").removeClass("masterDiv-theme3");
+    $(".masterDiv").addClass("masterDiv-theme1");
+
+    $("h3").removeClass("text-theme2");
+    $("h3").removeClass("text-theme3");
+    $("h3").addClass("text-theme1");
+
+    $("h2").removeClass("text-theme2");
+    $("h2").removeClass("text-theme3");
+    $("h2").addClass("text-theme1");
+
+    $("label").removeClass("text-theme2");
+    $("label").removeClass("text-theme3");
+    $("label").addClass("text-theme1");
+
+    $("span").removeClass("text-theme2");
+    $("span").removeClass("text-theme3");
+    $("span").addClass("text-theme1");
+
+    $("#input-range").removeClass("input-range-theme2");
+    $("#input-range").removeClass("input-range-theme3");
+    $("#input-range").addClass("input-range-theme1");
+
+    $("#displayDiv").removeClass("displayDiv-theme2");
+    $("#displayDiv").removeClass("displayDiv-theme3");
+    $("#displayDiv").addClass("displayDiv-theme1");
+
+    $("#inputDiv").removeClass("inputDiv-theme2");
+    $("#inputDiv").removeClass("inputDiv-theme3");
+    $("#inputDiv").addClass("inputDiv-theme1");
+
+    $(".numeric").removeClass("button-theme2");
+    $(".numeric").removeClass("button-theme3");
+    $(".numeric").addClass("button-theme1");
+
+    $(".delete").removeClass("delete-theme2");
+    $(".delete").removeClass("delete-theme3");
+    $(".delete").addClass("delete-theme1");
+
+    $(".reset").removeClass("reset-theme2");
+    $(".reset").removeClass("reset-theme3");
+    $(".reset").addClass("reset-theme1");
+
+    $(".equals").removeClass("equals-theme2");
+    $(".equals").removeClass("equals-theme3");
+    $(".equals").addClass("equals-theme1");
+
+
+
+  }
+
+  if ($("#input-range").val() === "2") {
+    console.log("boom, theme 2!");
+    $(".masterDiv").removeClass("masterDiv-theme1");
+    $(".masterDiv").removeClass("masterDiv-theme3");
+    $(".masterDiv").addClass("masterDiv-theme2");
+
+    $("h3").removeClass("text-theme1");
+    $("h3").removeClass("text-theme3");
+    $("h3").addClass("text-theme2");
+
+    $("h2").removeClass("text-theme1");
+    $("h2").removeClass("text-theme3");
+    $("h2").addClass("text-theme2");
+
+    $("label").removeClass("text-theme1");
+    $("label").removeClass("text-theme3");
+    $("label").addClass("text-theme2");
+
+    $("span").removeClass("text-theme1");
+    $("span").removeClass("text-theme3");
+    $("span").addClass("text-theme2");
+
+    $("#input-range").removeClass("input-range-theme1");
+    $("#input-range").removeClass("input-range-theme3");
+    $("#input-range").addClass("input-range-theme2");
+
+    $("#displayDiv").removeClass("displayDiv-theme1");
+    $("#displayDiv").removeClass("displayDiv-theme3");
+    $("#displayDiv").addClass("displayDiv-theme2");
+
+    $("#inputDiv").removeClass("inputDiv-theme1");
+    $("#inputDiv").removeClass("inputDiv-theme3");
+    $("#inputDiv").addClass("inputDiv-theme2");
+
+    $(".numeric").removeClass("button-theme1");
+    $(".numeric").removeClass("button-theme3");
+    $(".numeric").addClass("button-theme2");
+
+    $(".delete").removeClass("delete-theme1");
+    $(".delete").removeClass("delete-theme3");
+    $(".delete").addClass("delete-theme2");
+
+    $(".reset").removeClass("reset-theme1");
+    $(".reset").removeClass("reset-theme3");
+    $(".reset").addClass("reset-theme2");
+
+    $(".equals").removeClass("equals-theme1");
+    $(".equals").removeClass("equals-theme3");
+    $(".equals").addClass("equals-theme2");
+
+
+  }
+
+
+  if ($("#input-range").val() === "3") {
+    console.log("theme 3, hell yeah!");
+
+
+    $(".masterDiv").removeClass("masterDiv-theme1");
+    $(".masterDiv").removeClass("masterDiv-theme2");
+    $(".masterDiv").addClass("masterDiv-theme3");
+
+
+    $("h3").removeClass("text-theme2");
+    $("h3").removeClass("text-theme1");
+    $("h3").addClass("text-theme3");
+
+    $("h2").removeClass("text-theme2");
+    $("h2").removeClass("text-theme1");
+    $("h2").addClass("text-theme3");
+
+    $("label").removeClass("text-theme2");
+    $("label").removeClass("text-theme1");
+    $("label").addClass("text-theme3");
+
+    $("span").removeClass("text-theme2");
+    $("span").removeClass("text-theme1");
+    $("span").addClass("text-theme3");
+
+    $("#input-range").removeClass("input-range-theme2");
+    $("#input-range").removeClass("input-range-theme1");
+    $("#input-range").addClass("input-range-theme3");
+
+    $("#displayDiv").removeClass("displayDiv-theme2");
+    $("#displayDiv").removeClass("displayDiv-theme1");
+    $("#displayDiv").addClass("displayDiv-theme3");
+
+    $("#inputDiv").removeClass("inputDiv-theme2");
+    $("#inputDiv").removeClass("inputDiv-theme1");
+    $("#inputDiv").addClass("inputDiv-theme3");
+
+    $(".numeric").removeClass("button-theme2");
+    $(".numeric").removeClass("button-theme1");
+    $(".numeric").addClass("button-theme3");
+
+    $(".delete").removeClass("delete-theme2");
+    $(".delete").removeClass("delete-theme1");
+    $(".delete").addClass("delete-theme3");
+
+    $(".reset").removeClass("reset-theme2");
+    $(".reset").removeClass("reset-theme1");
+    $(".reset").addClass("reset-theme3");
+
+    $(".equals").removeClass("equals-theme2");
+    $(".equals").removeClass("equals-theme1");
+    $(".equals").addClass("equals-theme3");
+
+  }
+
+};
+
+
+
+
+
+
 
 
 
